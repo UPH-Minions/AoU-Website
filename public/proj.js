6c271529174044473fd9e14659c9ee6920079b31
RichardAngka <richangkawinata@gmail.com>
2021-02-08 14:34:35 +0700

    AoU

--- public/proj.js
+++ public/proj.js
@@ -12,3 +12,13 @@ $('.carousel.carousel-multi .carousel-item').each(function () {
 		$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
 	}
 });
+const parentContainer = document.querySelector('.read-more-container');
+parentContainer.addEventListener('click', event=>{
+	const current = event.target;
+	const isReadMoreBtn = current.className.includes('read-more-btn');
+	if(!isReadMoreBtn) return;
+	const currentText = event.target.parentNode.querySelector('.read-more-text');
+	currentText.classList.toggle('read-more-text--show');
+	current.textContent = current.textContent.includes('Read More') ?
+	"Read Less..." : "Read More...";
+})
